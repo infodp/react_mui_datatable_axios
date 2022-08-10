21521dbcdb10db1ef99d441db27ccfa8a2a7f016
zasalastobon <zas_91@hotmail.com>
2022-08-10 15:57:19 -0500

    Update App.js
    Creo una clase  para solo tener el tema oscuro, sin tener el tema dentro de la tabla de datos.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import { TableAxios } from './tables/TableAxios';
 import { TableBasic } from './tables/TableBasic';
 import { TableJson } from './tables/TableJson';
+import { ThemeDark } from "./layout/themeDark.js ";
 
 function App() {
   return (
-    <div className="App">
-     <TableBasic />
-     {/* <TableJson /> */}
-     {/* <TableAxios /> */}
+    <div className="App">     
+      {/* use data array */}
+      <TableBasic />
+      {/*  use data file json */}
+      <TableJson />
+      {/* apply dark style */}
+      <ThemeDark>
+        <TableAxios />
+      </ThemeDark>  
     </div>
   );
 }
